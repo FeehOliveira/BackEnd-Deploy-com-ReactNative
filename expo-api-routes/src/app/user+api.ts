f57f0396aa4114bc3ab4308259beb5fe4d35818e
Felipe Oliveira <feeholiveira10@outlook.com>
2024-03-06 03:57:27 -0300

    return error endpoint

--- expo-api-routes/src/app/user+api.ts
+++ expo-api-routes/src/app/user+api.ts
@@ -1,9 +1,22 @@
-import { ExpoResponse } from "expo-router/server"
+import { ExpoResponse, ExpoRequest } from "expo-router/server"
 
-export async function GET(): Promise<ExpoResponse> {
-    return ExpoResponse.json({
-        message: "resposta do backend mobile!",
+export async function POST(request: ExpoRequest): Promise<ExpoResponse> {
+    
+    const {email, password } = await request.json()
+
+    if(email === "feeholiveira@gmail.com" && password === "12345") {
+        return ExpoResponse.json({
+            email,
+            name: "Felipe Oliveira"
+        })
+
     }
 
-    )
+    return new ExpoResponse("User or password incorrect!", {
+        status: 404,
+        headers: {
+            "Content-Type": "text/plain",
+        }
+    })
+    
 }
