5a4614bcf9f22e9cabaea1b4effc8bf5240021e0
Felipe Oliveira <feeholiveira10@outlook.com>
2024-03-06 03:41:50 -0300

    send data to endpoint

---
+++ expo-api-routes/src/app/user+api.ts
@@ -0,0 +1,9 @@
+import { ExpoResponse } from "expo-router/server"
+
+export async function GET(): Promise<ExpoResponse> {
+    return ExpoResponse.json({
+        message: "resposta do backend mobile!",
+    }
+
+    )
+}
